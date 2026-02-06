475c363635deb070a54322ecdc9c87d673192263
jaocbhebbel <jacob.hebbel@gmail.com>
2026-02-06 16:36:56 -0500

    basic errors

---
+++ backend/src/utils/errors/errors.js
@@ -0,0 +1,24 @@
+import {
+    WebError,
+    InfraError,
+    ServiceError,
+    DataError
+} from './template.js';
+
+// children for WebError
+
+export class ValidationError extends WebError {
+    constructor() {};
+}
+
+export class HttpError extends WebError {
+    constructor() {};
+}
+
+export class AuthenticationError extends WebError {
+    constructor() {};
+}
+
+export class AuthorizationError extends WebError {
+    constructor() {};
+}
