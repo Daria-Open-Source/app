0a5190390b34f7df195d5582081fb2f053a8ce6b
jaocbhebbel <jacob.hebbel@gmail.com>
2026-02-09 18:08:58 -0500

    merge fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,4 +6,8 @@ app.get('/ping', (req, res) => {
     return res.status(200).json({ msg: 'pinged' });
 });
 
+app.get('/', (req, res) => {
+    return res.status(200).json({ msg: 'home' });
+});
+
 export default app;
